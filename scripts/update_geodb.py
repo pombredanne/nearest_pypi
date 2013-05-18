6b786b5a08fa03622f441829f57409a83b91ee0f
Ulrich Petri <ulo@ulo.pe>
2013-05-18 17:31:56 +0200

    fixed update_geodb script

--- scripts/update_geodb.py
+++ scripts/update_geodb.py
@@ -5,17 +5,22 @@ from config import Config
 class Command(object):
     def run(self):
 
-        subprocess.call([
-            'wget',
-            '-O',
-            Config.GEOIP_PATH_V4,
-            Config.GEOIP_DB_URL_IPV4,
-        ])
+        geodb_file_v4 = "{}.gz".format(Config.GEOIP_PATH_V4)
+        geodb_file_v6 = "{}.gz".format(Config.GEOIP_PATH_V6)
 
         subprocess.call([
             'wget',
-            '-O',
-            Config.GEOIP_PATH_V6,
+            '-O', geodb_file_v4,
+            Config.GEOIP_DB_URL_IPV4
+        ])
+        subprocess.call(["gunzip", "-f", geodb_file_v4])
+
+        subprocess.call([
+            'wget',
+            '-O', geodb_file_v6,
             Config.GEOIP_DB_URL_IPV6,
         ])
+        subprocess.call(["gunzip", "-f", geodb_file_v6])
 
+if __name__ == "__main__":
+    Command().run()
