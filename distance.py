be850d9d30a56b90efb1dc872f3bd3d925ff84ea
Ulrich Petri <ulo@ulo.pe>
2013-05-18 17:05:39 +0200

    Refactored distance calculation

--- distance.py
+++ distance.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from math import radians, cos, sin, asin, sqrt
 import socket
 from pygeoip import GeoIP, MEMORY_CACHE
@@ -13,15 +14,20 @@ FALLBACK_MIRROR = "a.pypi.python.org"
 MIRROR_KEY = "mirror:{}"
 
 
-class MirrorDistance(object):
+class GeoIPLookupError(Exception):
+    pass
+
+
+class DistanceCalculator(object):
     _geoip4 = None
     _geoip6 = None
 
     def __init__(self):
+        # Load the GeoIP databases into class attributes since they each need 20+ MB in memory
         if not self.__class__._geoip4:
-            self.__class__._geoip4 = GeoIP(Config.GEOIP_V4_PATH, MEMORY_CACHE)
+            self.__class__._geoip4 = GeoIP(Config.GEOIP_PATH_V4, MEMORY_CACHE)
         if not self.__class__._geoip6:
-            self.__class__._geoip6 = GeoIP(Config.GEOIP_V6_PATH, MEMORY_CACHE)
+            self.__class__._geoip6 = GeoIP(Config.GEOIP_PATH_V6, MEMORY_CACHE)
         self.redis = StrictRedis(Config.REDIS['HOST'], Config.REDIS['PORT'], Config.REDIS['DB'])
 
     @staticmethod
@@ -37,41 +43,43 @@ class MirrorDistance(object):
         dlat = lat2 - lat1
         a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
         c = 2 * asin(sqrt(a))
-        km = 6367 * c # convert to km
+        km = 6367 * c  # convert to km
         return km
 
-    def get_nearest_mirror(self, address):
+    def get_mirror_distances(self, address):
         key = MIRROR_KEY.format(address)
-        mirror = self.redis.get(key)
-        if not mirror:
+        distances = OrderedDict(self.redis.zrange(key, 0, -1, withscores=True, score_cast_func=int))
+        if not distances:
             try:
                 if ":" in address:
                     record = self._geoip6.record_by_addr(address)
                 else:
                     record = self._geoip4.record_by_addr(address)
             except socket.error:
-                return FALLBACK_MIRROR
+                raise GeoIPLookupError()
             if not record:
-                return FALLBACK_MIRROR
+                raise GeoIPLookupError()
             lat = record['latitude']
             lon = record['longitude']
 
-            distance = 99999
-            for candidate in Mirror.objects.filter(age__lt=3601):
-                if candidate.name == "a.pypi.pyton.org":
-                    # we don't want to use a.pypi in general
-                    continue
-                new_distance = self._haversine(lon, lat, candidate.lon, candidate.lat)
-                if new_distance < distance:
-                    distance = new_distance
-                    mirror = candidate.name
+            distances = OrderedDict(
+                (mirror.name, self._haversine(lon, lat, mirror.lon, mirror.lat))
+                for mirror in Mirror.objects.filter(age__lte=3600)
+            )
+            self.redis.zadd(key, **distances)
+            self.redis.expire(key, 60 * 60 * 24)  # one day
+        return distances
 
-            if mirror:
-                self.redis.set(key, mirror)
-                self.redis.expire(key, 60 * 60 * 24 * 120)  # 120 days
-        return mirror
+    def get_nearest_mirror(self, address):
+        try:
+            distances = self.get_mirror_distances(address)
+            if distances:
+                return next(distances.iteritems())[0]
+            return FALLBACK_MIRROR
+        except GeoIPLookupError:
+            return FALLBACK_MIRROR
 
 
 if __name__ == "__main__":
-    m = MirrorDistance()
+    m = DistanceCalculator()
     m.get_nearest_mirror("5.9.5.200")
--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from stdnet import odm
 from stdnet.odm.fields import SymbolField, CharField, IntegerField, DateTimeField
 from stdnet.odm.models import StdModel
 from config import Config
-from distance import MirrorDistance
 
 
 class Mirror(StdModel):
@@ -14,8 +13,18 @@ class Mirror(StdModel):
     def __unicode__(self):
         return u"{self.name} -> {self.lat}:{self.lon}".format(self=self)
 
-    def get_nearest_mirror(self, address):
-        return MirrorDistance().get_nearest_mirror(address)
+    @staticmethod
+    def get_nearest_mirror(address):
+        from distance import DistanceCalculator
+        return DistanceCalculator().get_nearest_mirror(address)
+
+    @staticmethod
+    def get_mirror_distances(address):
+        from distance import DistanceCalculator, GeoIPLookupError
+        try:
+            return DistanceCalculator().get_mirror_distances(address)
+        except GeoIPLookupError:
+            return {}
 
 
 odm.register(Mirror, Config.STDNET_DB_URL)
