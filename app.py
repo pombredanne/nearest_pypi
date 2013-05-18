32af1c707a737cfd3157231f3cb6cf525147d61f
Ulrich Petri <ulo@ulo.pe>
2013-05-18 17:06:15 +0200

    Added basic info page

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import sys
-import logging
 from urlparse import urlunsplit
 from flask.app import Flask
 from flask.globals import request
+from flask.templating import render_template
 from werkzeug.utils import redirect
-from distance import MirrorDistance
+from distance import FALLBACK_MIRROR
 from models import Mirror
 from logging import getLogger
 
@@ -17,13 +16,23 @@ app.config.from_object("config.Config")
 
 @app.route("/")
 def index():
-    ret = str(list(Mirror.objects.filter(age__lt=3601)))
-    return "ok " + ret
+    distances = Mirror.get_mirror_distances(request.remote_addr)
+    context = {
+        'ip': request.remote_addr,
+    }
+    if distances:
+        nearest_mirror = next(distances.iteritems())
+        context['mirror'] = nearest_mirror[0]
+        context['mirror_distance'] = nearest_mirror[1]
+    else:
+        context['no_mirror'] = True
+        context['fallback_mirror'] = FALLBACK_MIRROR
+    return render_template("index.html", **context)
 
 
 @app.route("/simple/<path:path>")
 def proxy(path):
-    mirror = MirrorDistance().get_nearest_mirror(request.remote_addr)
+    mirror = Mirror.get_nearest_mirror(request.remote_addr)
     url = urlunsplit((
         "http",
         mirror,
